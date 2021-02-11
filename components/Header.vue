a574a6f9921241caf8d2fd769c9fd61288ddfea1
Julia Doroshina <up.doroshina@yandex.ru>
2021-02-11 15:38:53 +0300

    refactor: extract header

---
+++ components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="header">
+    <Logo />
+    <div class="header__info">
+      <p class="header__title">Ходов</p>
+      <p class="header__text">
+        <slot name="steps" />
+      </p>
+    </div>
+    <div class="header__info">
+      <p class="header__title">Время</p>
+      <p class="header__text">
+        <slot name="time" />
+      </p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+  }
+
+  .header__info {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    min-width: 90px;
+    min-height: 75px;
+    padding: 10px 5px;
+    margin-left: 10px;
+    box-sizing: border-box;
+    background-color: $dark-purple;
+    border-radius: $border-radius;
+    color: $light-text;
+  }
+
+  .header__title {
+    font-size: 16px;
+  }
+
+  .header__text {
+    font-size: 20px;
+    font-weight: 500;
+  }
+
+  .header__info:first-of-type {
+    margin-left: 0;
+  }
+
+</style>
--- components/Widget.vue
+++ components/Widget.vue
@@ -1,17 +1,10 @@
 <template>
   <div class="container">
     <button class="button" @click="startGame">Новая игра</button>
-    <div class="header">
-      <Logo />
-      <div class="header__info">
-        <p class="header__title">Ходов</p>
-        <p class="header__text">0</p>
-      </div>
-      <div class="header__info">
-        <p class="header__title">Время</p>
-        <p class="header__text">{{ formatedTime }}</p>
-      </div>
-    </div>
+    <Header>
+      <template v-slot:steps>{{ steps }}</template>
+      <template v-slot:time>{{ formatedTime }}</template>
+    </Header>
     <Puzzle />
   </div>
 </template>
@@ -20,6 +13,8 @@
   export default {
     data() {
       return {
+        steps: 0,
+        //  Для таймера
         currentTime: 0,
         maxTime: 900,
         counter: false,
@@ -96,40 +91,6 @@
 
   .button:active {
     box-shadow: none;
-  }
-
-  .header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 20px;
-  }
-
-  .header__info {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    min-width: 90px;
-    min-height: 75px;
-    padding: 10px 5px;
-    margin-left: 10px;
-    box-sizing: border-box;
-    background-color: $dark-purple;
-    border-radius: $border-radius;
-    color: $light-text;
-  }
-
-  .header__title {
-    font-size: 16px;
-  }
-
-  .header__text {
-    font-size: 20px;
-    font-weight: 500;
-  }
-
-  .header__info:first-of-type {
-    margin-left: 0;
   }
 
 </style>
